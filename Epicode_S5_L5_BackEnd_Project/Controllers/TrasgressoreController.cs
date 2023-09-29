b2ad322236aa051dbfb96c553456367885d55e1c
girardiantonio <134281701+girardiantonio@users.noreply.github.com>
2023-09-29 23:50:48 +0200

    Aggiunto Modifca

--- Epicode_S5_L5_BackEnd_Project/Controllers/TrasgressoreController.cs
+++ Epicode_S5_L5_BackEnd_Project/Controllers/TrasgressoreController.cs
@@ -16,6 +16,39 @@ namespace Epicode_S5_L5_BackEnd_Project.Controllers
             return ConfigurationManager.ConnectionStrings["DB_ConnString"].ConnectionString;
         }
 
+        private Trasgressore GetTrasgressoreById(int IdAnagrafica)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(GetConnectionString()))
+            {
+                sqlConnection.Open();
+                string query = "SELECT * FROM Anagrafica WHERE IdAnagrafica = @IdAnagrafica";
+
+                using (SqlCommand cmd = new SqlCommand(query, sqlConnection))
+                {
+                    cmd.Parameters.AddWithValue("@IdAnagrafica", IdAnagrafica);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            Trasgressore trasgressore = new Trasgressore
+                            {
+                                IdAnagrafica = (int)reader["IdAnagrafica"],
+                                Cognome = reader["Cognome"].ToString(),
+                                Nome = reader["Nome"].ToString(),
+                                Indirizzo = reader["Indirizzo"].ToString(),
+                                Citta = reader["Citta"].ToString(),
+                                Cap = reader["Cap"].ToString(),
+                                Codice = reader["Codice"].ToString()
+                            };
+                            return trasgressore;
+                        }
+                        return null;
+                    }
+                }
+            }
+        }
+
         [HttpGet]
         public ActionResult ListaTrasgressori()
         {
@@ -117,40 +150,54 @@ namespace Epicode_S5_L5_BackEnd_Project.Controllers
             return RedirectToAction("ListaTrasgressori");
         }
 
-
-        private Trasgressore GetTrasgressoreById(int IdAnagrafica)
+        [HttpGet]
+        public ActionResult ModificaTrasgressore(int IdAnagrafica)
         {
-            using (SqlConnection sqlConnection = new SqlConnection(GetConnectionString()))
+            Trasgressore trasgressoreDaModificare = GetTrasgressoreById(IdAnagrafica);
+
+            if (trasgressoreDaModificare == null)
             {
-                sqlConnection.Open();
-                string query = "SELECT * FROM Anagrafica WHERE IdAnagrafica = @IdAnagrafica";
-
-                using (SqlCommand cmd = new SqlCommand(query, sqlConnection))
-                {
-                    cmd.Parameters.AddWithValue("@IdAnagrafica", IdAnagrafica);
-
-                    using (SqlDataReader reader = cmd.ExecuteReader())
-                    {
-                        if (reader.Read())
-                        {
-                            Trasgressore trasgressore = new Trasgressore
-                            {
-                                IdAnagrafica = Convert.ToInt32(reader["IdAnagrafica"]),
-                                Cognome = reader["Cognome"].ToString(),
-                                Nome = reader["Nome"].ToString(),
-                                Indirizzo = reader["Indirizzo"].ToString(),
-                                Citta = reader["Citta"].ToString(),
-                                Cap = reader["Cap"].ToString(),
-                                Codice = reader["Codice"].ToString()
-                            };
-                            return trasgressore;
-                        }
-                        return null;
-                    }
-                }
+                TempData["Errore"] = "Trasgressore non trovato!";
             }
+
+            return View(trasgressoreDaModificare);
         }
 
+        [HttpPost]
+        public ActionResult ModificaTrasgressore(Trasgressore trasgressoreModificato)
+        {
+            if (ModelState.IsValid)
+            {
+                string query = "UPDATE Anagrafica SET " +
+                    "Cognome = @Cognome, " +
+                    "Nome = @Nome, " +
+                    "Indirizzo = @Indirizzo, " +
+                    "Citta = @Citta, " +
+                    "Cap = @Cap, " +
+                    "Codice = @Codice " +
+                    "WHERE IdAnagrafica = @IdAnagrafica";
+
+                using (SqlConnection sqlConnection = new SqlConnection(GetConnectionString()))
+                {
+                    sqlConnection.Open();
+
+                    using (SqlCommand cmd = new SqlCommand(query, sqlConnection))
+                    {
+                        cmd.Parameters.AddWithValue("@IdAnagrafica", trasgressoreModificato.IdAnagrafica);
+                        cmd.Parameters.AddWithValue("@Cognome", trasgressoreModificato.Cognome);
+                        cmd.Parameters.AddWithValue("@Nome", trasgressoreModificato.Nome);
+                        cmd.Parameters.AddWithValue("@Indirizzo", trasgressoreModificato.Indirizzo);
+                        cmd.Parameters.AddWithValue("@Citta", trasgressoreModificato.Citta);
+                        cmd.Parameters.AddWithValue("@Cap", trasgressoreModificato.Cap);
+                        cmd.Parameters.AddWithValue("@Codice", trasgressoreModificato.Codice);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                TempData["Messaggio"] = "Trasgressore modificato con successo!";
+            }
+            return RedirectToAction("ListaTrasgressori");
+        }
 
 
 
